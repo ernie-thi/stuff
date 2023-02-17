f05af8bbceae388ca48e26d5f6bbfc0c44f37fbe
Raphael Niederauer <rapnie96@gmail.com>
2023-02-17 15:12:50 +0100

    install.sh: not finished yet

---
+++ bash/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### Which package manager to use
+PKG=''
+if [ -x "$(command -v apt)" ]; then
+    echo -n "apt Paketmanager"
+    PKG="apt"
+elif [ -x "$(command -v dnf)" ]; then 
+    echo  "dnf Paketmanager"
+    PKG="dnf"
+else 
+    echo "Paketverwaltungssystem nicht gefunden"
+fi
+
+### Update Packages
+
+echo "Following: Update packages"
+{ sudo $PKG update; sudo $PKG upgrade; }
+echo "Updating packages completed successfully"
+
+# TODO:  <17-02-23, programme die zu installieren sind einfügen> #
