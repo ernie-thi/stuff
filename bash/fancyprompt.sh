6e674efd7168be7275e8c6689d3f48fa6e1f2c74
Raphael Niederauer <rapnie96@gmail.com>
2023-02-18 14:40:04 +0100

    changed paths

--- bash/fancyprompt.sh
+++ bash/fancyprompt.sh
@@ -12,13 +12,15 @@ sudo $PKG install -y powerline-fonts
 echo "Powerline-fonts installiert"
 
 # git clone synth-shell repo
+cd /home/$USER
 git clone https://github.com/andresgongora/synth-shell.git
 chmod +x synth-shell/setup.sh
 cd synth-shell
 ./setup.sh
 
 # transferring config file to .config
-cp ../dotfiles/synth-shell-prompt.config /home/$USER/.config/synth-shell/
+cd /home/$USER/stuff
+cp dotfiles/synth-shell-prompt.config /home/$USER/.config/synth-shell/
 
 # Message to confirm installation
 echo "SynthShell wurde installiert (wirksam nach Shell Neustart)"
