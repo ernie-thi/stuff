afc0ea26a34d754afbc96914cd76247ab85831b9
Raphael Niederauer <rapnie96@gmail.com>
2023-02-18 18:44:54 +0100

    make it nicer

--- bash/fancyprompt.sh
+++ bash/fancyprompt.sh
@@ -1,27 +1,57 @@
 #!/bin/bash
 
-# determine correct package manager command
-if [ -x "$(command -v apt)" ]; then
+# determine correct package manager 
+if command -v apt 
+then
     PKG="apt"
-elif [ -x "$(command -v dnf)" ]; then
+elif command -v dnf 
+then
     PKG="dnf"
 fi
 
-# install powerline fonts
-sudo $PKG install -y powerline-fonts
-echo "Powerline-fonts installiert"
+# Creating logfile
+log="/home/$USER/log_fancyprompt.txt"
+touch $log
+
+# check for install powerline fonts or do so
+if ( $PKG list --installed powerline-fonts || $PKG list --installed fonts-powerline )
+then
+    echo "Powerline-fonts sind bereits installiert" >> $log
+else
+    sudo $PKG install -y powerline-fonts || sudo $PKG install -y fonts-powerline
+    if [ $? -eq 0 ]
+    then
+        echo "Powerline-fonts wurden erfolgreich installiert" >> $log
+    else
+        echo "Powerline-fonts konnten nicht erfolgreich installiert werden" >> $log
+    fi
+fi
 
 # git clone synth-shell repo
 cd /home/$USER
-git clone --recursive https://github.com/andresgongora/synth-shell.git
-chmod +x synth-shell/setup.sh
-cd synth-shell
-./setup.sh
+if [ ! -d ~/synth-shell ]
+then
+    git clone --recursive https://github.com/andresgongora/synth-shell.git
+    if [ $? -eq 0 ]
+    then
+        echo "Synthshell erfolgreich geklont" >> $log
+        chmod +x synth-shell/setup.sh
+        cd synth-shell
+        # run setup script
+        ./setup.sh
+        if [ $? -eq 0 ]
+        then
+            echo "setup.sh wurde erfolgreich beendet" >> $log
+            cd /home/$USER/stuff
+            cp ./dotfiles/synth-shell-prompt.config /home/$USER/.config/synth-shell/
+        else
+            echo "setup.sh konnte nicht erfolgreich ausgeführt werden" >> $log
+        fi
+    else
+        echo "Synthshell nicht erfolgreich geklont" >> $log
+    fi
+else
+    echo "Synth-Shell Dir existiert bereits" >> $log
+fi
 
-# transferring config file to .config
-cd /home/$USER/stuff
-#mkdir /home/$USER/.config/synth-shell
-cp ./dotfiles/synth-shell-prompt.config /home/$USER/.config/synth-shell/
-
-# Message to confirm installation
-echo "SynthShell wurde installiert (wirksam nach Shell Neustart)"
+echo "fancyprompt script beendet" >> $log
