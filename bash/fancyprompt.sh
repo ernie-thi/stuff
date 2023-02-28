0144e9417cd254878c3eba656b4af39d4593c819
Raphael Niederauer <rapnie96@gmail.com>
2023-02-28 18:22:41 +0100

    add comments; change errorlog and logfile paths

--- bash/fancyprompt.sh
+++ bash/fancyprompt.sh
@@ -10,48 +10,48 @@ then
 fi
 
 # Creating logfile
-log="/home/$USER/log_fancyprompt.txt"
-touch $log
+logfile="/home/$USER/log_fancyprompt.log"
+errorlog="$HOME/fancyprompt_error.log"
 
 # check for install powerline fonts or do so
-if ( $PKG list --installed powerline-fonts || $PKG list --installed fonts-powerline )
+if ( $PKG list --installed powerline-fonts || $PKG list --installed fonts-powerline ) # check wheter correct fonts are yet installed
 then
-    echo "Powerline-fonts sind bereits installiert" >> $log
+    echo "Powerline-fonts sind bereits installiert" >> $logfile
 else
-    sudo $PKG install -y powerline-fonts || sudo $PKG install -y fonts-powerline
+    sudo $PKG install -y powerline-fonts || sudo $PKG install -y fonts-powerline    # install fonts if necessary
     if [ $? -eq 0 ]
     then
-        echo "Powerline-fonts wurden erfolgreich installiert" >> $log
+        echo "Powerline-fonts wurden erfolgreich installiert" >> $logfile
     else
-        echo "Powerline-fonts konnten nicht erfolgreich installiert werden" >> $log
+        echo "Powerline-fonts konnten nicht erfolgreich installiert werden" >> $errorlog
     fi
 fi
 
 # git clone synth-shell repo
 cd /home/$USER
-if [ ! -d ~/synth-shell ]
+if [ ! -d ~/synth-shell ]       # check for existence of synthshell
 then
-    git clone --recursive https://github.com/andresgongora/synth-shell.git
+    git clone --recursive https://github.com/andresgongora/synth-shell.git      # if not so, clone it
     if [ $? -eq 0 ]
     then
-        echo "Synthshell erfolgreich geklont" >> $log
-        chmod +x synth-shell/setup.sh
-        cd synth-shell
+        echo "Synthshell erfolgreich geklont" >> $logfile
+        chmod +x synth-shell/setup.sh       # make it executable
+        cd synth-shell                      # change dir
         # run setup script
         ./setup.sh
         if [ $? -eq 0 ]
         then
-            echo "setup.sh wurde erfolgreich beendet" >> $log
-            cd /home/$USER/stuff
-            cp ./dotfiles/synth-shell-prompt.config /home/$USER/.config/synth-shell/
+            echo "setup.sh wurde erfolgreich beendet" >> $logfile
+            cd /home/$USER/stuff        # switch to correct dir
+            cp ./dotfiles/synth-shell-prompt.config /home/$USER/.config/synth-shell/ # copy template of config
         else
-            echo "setup.sh konnte nicht erfolgreich ausgeführt werden" >> $log
+            echo "setup.sh konnte nicht erfolgreich ausgeführt werden" >> $errorlog
         fi
     else
-        echo "Synthshell nicht erfolgreich geklont" >> $log
+        echo "Synthshell nicht erfolgreich geklont" >> $errorlog
     fi
 else
-    echo "Synth-Shell Dir existiert bereits" >> $log
+    echo "Synth-Shell Dir existiert bereits" >> $logfile
 fi
 
-echo "fancyprompt script beendet" >> $log
+echo "fancyprompt script beendet" >> $logfile
