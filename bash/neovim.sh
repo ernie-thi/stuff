3cb6596d4be1c5e0ff209e5b638ef5bf325c2fb1
Raphael Niederauer <rapnie96@gmail.com>
2023-03-04 20:45:06 +0100

    change curl command

--- bash/neovim.sh
+++ bash/neovim.sh
@@ -65,8 +65,10 @@ else
 fi
 
 ## install vim-Plug Pluginmanager
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \ 
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \ 
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if [ -$? -eq 0 ]
 then    
