981c7caa13ed37ea9062fc5a897b0d78a64ebc6c
Raphael Niederauer <rapnie96@gmail.com>
2023-03-05 21:07:15 +0100

    differ package name

--- bash/fancyprompt.sh
+++ bash/fancyprompt.sh
@@ -31,9 +31,15 @@ fi
 #then
 #    echo "Powerline-fonts sind bereits installiert" >> $logfile
 #else
-    sudo $PKG install -y powerline-fonts || sudo $PKG install -y fonts-powerline >>$logfile 2>>$errorlog    # install fonts if necessary
+if [[ "$PKG" == "dnf" ]]; then
+    sudo $PKG install -y powerline-fonts >>$logfile 2>>$errorlog
     check_exit_status "powerline-fonts"
-#fi
+elif [[ "$PKG" == "apt" ]]; then
+    sudo $PKG install -y fonts-powerline >>$logfile 2>>$errorlog    # install fonts if necessary
+    check_exit_status "powerline-fonts"
+else
+    echo "Packagemanager nicht erkannt!" >> $logfile
+fi
 
 # git clone synth-shell repo
 cd $HOME
