48ce3ebd0d6bea21916d7ea9704327869103ba05
ernie-thi <115615771+ernie-thi@users.noreply.github.com>
2023-04-09 14:13:04 +0000

    Adding some lines (codespaces --> not tested)

--- python/pdf_duplicates.py
+++ python/pdf_duplicates.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3 
 """ Find all pdf files in a directory and eliminate duplicates based on file Name. Using regex"""
 
-import os, shutil
+import os, shutil, sys
 from pathlib import Path
 p = Path.home()
 
 print(p)
 print(type(p))
+if sys.argv[1] == '':
+    sys.exit("Include path to folder which contains pdf files as first argument passed when calling")
+
+destination = str(sys.argv[1])
+os.chdir(destination)
+files = os.listdir()
+print("files: ------------")
+[print(i) for i in files]
