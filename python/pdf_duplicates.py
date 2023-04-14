df37bc5c085f48cbbe9cdcd619895bfd73d57127
Raphael Niederauer <rapnie96@gmail.com>
2023-04-14 20:45:01 +0200

    pdf_duplicates.py: massive logic changes, comments
    deleted unneccessary logic, add comments

--- python/pdf_duplicates.py
+++ python/pdf_duplicates.py
@@ -7,31 +7,47 @@ import sys
 import re
 from pathlib import Path
 
+# Grab home directory and directory from which filtering to be executed
 home = Path.home()
-destination = input('Input destination Folder to search for pdf duplicates, like $HOME/your/path: ')
+destination = str(input('Input destination Folder to search for pdf duplicates, like $HOME/your/path: ') or "/home/raphael/Downloads/")
 #if len(sys.argv) == 1:
 #    sys.exit("Include path to folder which contains pdf files as first argument passed when calling")
 #destination = str(sys.argv[1])
-if not destination:
+# In case no path given
+if not destination:             
     sys.exit("No Path inserted, Program terminated.")
+# change to destination directory and grab containing files
 os.chdir(destination)
 files = os.listdir()
 print("files: ------------")
-[print(i) for i in files]
+[print(i) for i in files]       # kontroll ausgabe
 
+# declaring empty list for pdf files
 pdf_files = []
-pattern = re.compile(r'([A-Za-z0-9]+.+)\.pdf$')
+pattern = re.compile(r'([A-Za-z0-9]+.+)(\s\(\d+\))*\.pdf$', re.IGNORECASE)
 for file in files:
-    #if re.search(r'\.pdf$', file) and :
-    if pattern.search(file) and pattern.sub(r'\1',file):
-        pdf_files.append(file)
-
+    if pattern.search(file):
+        pdf_files.append(file)          #appending pdf file to list
 print("""PDF-Files:--------------------------------------------------------------------------------------------------------------------------
         -----------------------------------------------------------------------------""")
-secondPdfs = []
-[ print(i) for i in pdf_files ]
-print(len(pdf_files))
-# TODO: pdf files filtern  <12-04-23,ernie> #
-# TODO: pdf files auf duplikate pruefen <12-04-23,ernie> #
-# TODO: dann nur noch die betroffenen files an send2trash senden <12-04-23,ernie> #
-# TODO: und die nicht betroffenen in ein zip packen <12-04-23,ernie> #
+pdf_files.sort()
+[print(i) for i in pdf_files]
+# declare a dict for pdf 
+pdf_dict = {}
+pattern_dups = re.compile(r'([A-Za-z0-9]+.+)(\s\(\d+\))$', re.IGNORECASE)
+for file in pdf_files:
+    basename, ext = os.path.splitext(file)
+    if pattern_dups.match(basename):
+        # online save filename without duplicate counter
+        original_basename = pattern_dups.match(basename).groups()[0]
+        # if filename is already existent in our dictionary add it to key:list
+        # (the full duplicate file name)
+        if original_basename in pdf_dict:
+            pdf_dict[original_basename].append(file)
+    else:
+        # kein Duplikat, erste Instanz
+        pdf_dict[basename] = [file]
+
+for key,value in pdf_dict.items():
+    print(f"{key} -----> {len(value)}")
+# TODO: ordner mit duplikaten erstellen oder zu send2trash senden <12-04-23,ernie> #
