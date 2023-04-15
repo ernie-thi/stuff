2578dad6aba34cc0c66f65d46973412c208fee94
Raphael Niederauer <rapnie96@gmail.com>
2023-04-15 13:56:35 +0200

    pdf_duplicates.py: multiple major changes - added a more detailed docstring at beginning - importing time module for debug reasons - change duplicate logic: is correct now - adding various prints for debugging reasons - add logic to move duplicate files - add logic to filter duplicates on file size

--- python/pdf_duplicates.py
+++ python/pdf_duplicates.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3 
-""" Find all pdf files in a directory and eliminate duplicates based on file Name. Using regex"""
+""" Find all pdf files in a directory and eliminate duplicates based on file
+Name. Using regex. Also create a folder in which duplicates will be moved, to
+restore them in case sth went wrong. This folder has to be deleted manually
+after the program ran. Also among all instances of one file, the one with the
+biggest size will be kept """
 
-import os 
+import os, time
 import shutil 
 import sys
 import re
@@ -25,29 +29,59 @@ print("files: ------------")
 # declaring empty list for pdf files
 pdf_files = []
 pattern = re.compile(r'([A-Za-z0-9]+.+)(\s\(\d+\))*\.pdf$', re.IGNORECASE)
+basenamepattern = re.compile(r'(\s\(\d+\))$')
 for file in files:
     if pattern.search(file):
         pdf_files.append(file)          #appending pdf file to list
 print("""PDF-Files:--------------------------------------------------------------------------------------------------------------------------
         -----------------------------------------------------------------------------""")
-pdf_files.sort()
+pdf_files = list([str(i) for i in pdf_files])
 [print(i) for i in pdf_files]
 # declare a dict for pdf 
+print("-------------------SORTIEREN---------------------------------------------------------------------------------")
 pdf_dict = {}
-pattern_dups = re.compile(r'([A-Za-z0-9]+.+)(\s\(\d+\))$', re.IGNORECASE)
+basenames= []
+#pattern_dups = re.compile(r'((.+)(\s\(\d+\))$', re.IGNORECASE)
 for file in pdf_files:
     basename, ext = os.path.splitext(file)
-    if pattern_dups.match(basename):
+    basenames.append(basename) 
+    match = pattern.search(file)
+    if match:
         # online save filename without duplicate counter
-        original_basename = pattern_dups.match(basename).groups()[0]
+        original_basename = re.sub(basenamepattern,'',basename)
+        print(original_basename)
+        #time.sleep(5)
         # if filename is already existent in our dictionary add it to key:list
         # (the full duplicate file name)
         if original_basename in pdf_dict:
             pdf_dict[original_basename].append(file)
-    else:
-        # kein Duplikat, erste Instanz
-        pdf_dict[basename] = [file]
+            print(f"{original_basename} appended to {file}")
+        else:
+            pdf_dict[original_basename] = [file]
 
+[print(i) for i in basenames]
+#pdf_files.sort()
+print("KEY VALUE PAARE -----------------------------------------------------------------------------------------------")
 for key,value in pdf_dict.items():
     print(f"{key} -----> {len(value)}")
+# Create duplicate Directory or check if already existing
+duplicate_path = os.path.join(destination, "Duplikate_pdfs")
+if not os.path.exists(duplicate_path):
+    os.makedirs(duplicate_path)
 # TODO: ordner mit duplikaten erstellen oder zu send2trash senden <12-04-23,ernie> #
+
+# Iterate over pdf_dict
+for key in pdf_dict.keys():
+    if len(pdf_dict[key]) > 1:
+        # determining largest file of lists
+        largest_file = max(pdf_dict[key], key=lambda x: os.path.getsize(x))
+        print(f"der größte ist: {largest_file}")
+        # moving the smaller files to dup folder
+        for smaller in pdf_dict[key]:
+            if not largest_file:
+                #shutil.move(smaller, os.path.join(duplicate_path, smaller))
+                print(f"Es wurde File {smaller} verschoben") 
+
+print(f"Duplicate pdf files moved to : {duplicate_path}")
+#TODO: fix last errors
+# funktioniert bis jetzt so 
