2b8615e7255bf70a25009986be34d71e3471b5a1
Raphael Niederauer <rapnie96@gmail.com>
2023-04-15 14:52:30 +0200

    pdf_duplicates.py: add asking to move, comments - added logic to ask before moving the duplicate files to respective   folder. Using move_duplicates variable and the respective logic   block. - also changed some comments

--- python/pdf_duplicates.py
+++ python/pdf_duplicates.py
@@ -15,15 +15,16 @@ from pathlib import Path
 home = Path.home()
 destination = str(input('Input destination Folder to search for pdf duplicates, like $HOME/your/path: ') or "/home/raphael/Downloads/")
 duplicate_exist = 0
+move_duplicates = 0
 
 # In case no path given
 if not destination:             
     sys.exit("No Path inserted, Program terminated.")
+
 # change to destination directory and grab containing files
 os.chdir(destination)
 files = os.listdir()
 print("files: ------------")
-#[print(i) for i in files]       # kontroll ausgabe
 
 # declaring empty list for pdf files and regex patterns
 pdf_files = []
@@ -36,7 +37,7 @@ for file in files:
 print("""FOUND PDF-Files:---------------------------------------------------------------------------------------""")
 [print(pdf) for pdf in pdf_files]
 
-# declare a dict for pdf 
+# declare a dict for pdf and sorting process
 print("--------------------------------------SORTING--------------------------------------------------------------")
 pdf_dict = {}
 for file in pdf_files:
@@ -48,7 +49,6 @@ for file in pdf_files:
         # if filename is already existent in our dictionary add it to key:list
         if original_basename in pdf_dict:
             pdf_dict[original_basename].append(file)
-        
         # when filename is not part of dict yet, add it as new entry
         else:
             pdf_dict[original_basename] = [file]
@@ -59,7 +59,7 @@ for key,value in pdf_dict.items():
         duplicate_exist += 1
         if duplicate_exist == 1:
             print("KEY VALUE PAIRS: -----------------------------------------------------------------------------------------------")
-        print(f"{key} ------------------------------------------------------------> {len(value)}")
+        print(f"{key} ----------------------------------> {len(value)}")
 
 #TODO: Maybe also print a statement to inform the user (in case) that no duplicate file was found at all
 if not duplicate_exist:
@@ -70,17 +70,22 @@ duplicate_path = os.path.join(destination, "Duplikate_pdfs")
 if not os.path.exists(duplicate_path):
     os.makedirs(duplicate_path)
 
-# Iterate over pdf_dict
-for key in pdf_dict.keys():
-    if len(pdf_dict[key]) > 1:
-        # determining largest file of lists
-        largest_file = max(pdf_dict[key], key=lambda x: os.path.getsize(x))
-        print(f"der größte ist: {largest_file}")
-        # moving the smaller files to dup folder
-        for smaller in pdf_dict[key]:
-            if smaller != largest_file:
-                shutil.move(smaller, os.path.join(duplicate_path, smaller))
-                print(f"Es wurde File {smaller} verschoben") 
-
-print(f"Duplicate pdf files moved to : {duplicate_path}")
+# Iterate over pdf_dict and ask user whether duplicate files should be moved then
+move_duplicates = int(input("Do you want to move the duplicates to a seperate folder now? Insert 1 for YES and 0 for NO: "))
+if move_duplicates:
+    for key in pdf_dict.keys():
+        if len(pdf_dict[key]) > 1:
+            # determining largest file of lists
+            largest_file = max(pdf_dict[key], key=lambda x: os.path.getsize(x))
+            print(f"der größte ist: {largest_file}")
+            # moving the smaller files to dup folder
+            for smaller in pdf_dict[key]:
+                if smaller != largest_file:
+                    shutil.move(smaller, os.path.join(duplicate_path, smaller))
+                    print(f"Es wurde File {smaller} verschoben") 
+    print(f"Duplicate pdf files moved to : {duplicate_path}")
+else:
+    print("Duplicate files NOT moved")
 #TODO: Maybe add Date of creation to duplicate folder, or log file which tells when programm last ran
+#TODO: Add Exception-Handling?
+#TODO: Add logging logic
