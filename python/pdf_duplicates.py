c90844824dcebbec68778ecc03981382c604a04f
Raphael Niederauer <rapnie96@gmail.com>
2023-04-15 14:31:52 +0200

    pdf_duplicates.py: tidying up, modifications - deleting dead and not needed code - deleting import of time module because not further needed - Add duplicate_exist: var to determine if there are duplicate files   and if true it will be not 0 - add logic to abort program if there are no duplicates, with message   to user. (Using duplicate_exist) Also only print Key_Value pairs if   there are duplicates.

--- python/pdf_duplicates.py
+++ python/pdf_duplicates.py
@@ -5,7 +5,7 @@ restore them in case sth went wrong. This folder has to be deleted manually
 after the program ran. Also among all instances of one file, the one with the
 biggest size will be kept """
 
-import os, time
+import os
 import shutil 
 import sys
 import re
@@ -14,9 +14,8 @@ from pathlib import Path
 # Grab home directory and directory from which filtering to be executed
 home = Path.home()
 destination = str(input('Input destination Folder to search for pdf duplicates, like $HOME/your/path: ') or "/home/raphael/Downloads/")
-#if len(sys.argv) == 1:
-#    sys.exit("Include path to folder which contains pdf files as first argument passed when calling")
-#destination = str(sys.argv[1])
+duplicate_exist = 0
+
 # In case no path given
 if not destination:             
     sys.exit("No Path inserted, Program terminated.")
@@ -26,44 +25,45 @@ files = os.listdir()
 print("files: ------------")
 #[print(i) for i in files]       # kontroll ausgabe
 
-# declaring empty list for pdf files
+# declaring empty list for pdf files and regex patterns
 pdf_files = []
 pattern = re.compile(r'([A-Za-z0-9]+.+)(\s\(\d+\))*\.pdf$', re.IGNORECASE)
 basenamepattern = re.compile(r'(\s\(\d+\))$')
+
 for file in files:
     if pattern.search(file):
         pdf_files.append(file)          #appending pdf file to list
-print("""PDF-Files:--------------------------------------------------------------------------------------------------------------------------
-        -----------------------------------------------------------------------------""")
-#pdf_files = list([str(i) for i in pdf_files])
-#[print(i) for i in pdf_files]
+print("""FOUND PDF-Files:---------------------------------------------------------------------------------------""")
+[print(pdf) for pdf in pdf_files]
+
 # declare a dict for pdf 
-print("-------------------SORTIEREN---------------------------------------------------------------------------------")
+print("--------------------------------------SORTING--------------------------------------------------------------")
 pdf_dict = {}
-basenames= []
-#pattern_dups = re.compile(r'((.+)(\s\(\d+\))$', re.IGNORECASE)
 for file in pdf_files:
     basename, ext = os.path.splitext(file)
-    basenames.append(basename) 
     match = pattern.search(file)
     if match:
-        # online save filename without duplicate counter
+        # save filename without duplicate counter
         original_basename = re.sub(basenamepattern,'',basename)
-        print(original_basename)
-        #time.sleep(5)
         # if filename is already existent in our dictionary add it to key:list
-        # (the full duplicate file name)
         if original_basename in pdf_dict:
             pdf_dict[original_basename].append(file)
-            print(f"{original_basename} appended to {file}")
+        
+        # when filename is not part of dict yet, add it as new entry
         else:
             pdf_dict[original_basename] = [file]
 
-#[print(i) for i in basenames]
-#pdf_files.sort()
-print("KEY VALUE PAARE -----------------------------------------------------------------------------------------------")
 for key,value in pdf_dict.items():
-    print(f"{key} -----> {len(value)}")
+    # To determine whether there were duplicates
+    if len(value) > 1:
+        duplicate_exist += 1
+        if duplicate_exist == 1:
+            print("KEY VALUE PAIRS: -----------------------------------------------------------------------------------------------")
+        print(f"{key} ------------------------------------------------------------> {len(value)}")
+
+#TODO: Maybe also print a statement to inform the user (in case) that no duplicate file was found at all
+if not duplicate_exist:
+    sys.exit("No duplicate PDF-Files, programm aborted")
 
 # Create duplicate Directory or check if already existing
 duplicate_path = os.path.join(destination, "Duplikate_pdfs")
