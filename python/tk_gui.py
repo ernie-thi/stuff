02d362f6119312b35f00a78ae2c44d4db0c67de7
ernie <rapnie96@gmail.com>
2023-10-21 15:25:52 +0200

    tkinter gui

--- python/tk_gui.py
+++ python/tk_gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk 
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import pdf_duplicates
 
 class PDFDuplicatesGUI(tk.Tk):
@@ -12,16 +12,22 @@ class PDFDuplicatesGUI(tk.Tk):
         self.label = tk.Label(self, text="Select a directory to find duplicates")
         self.label.pack(pady=20)
         
-        self.browse_button = tk.Button(self, text="Browse", command=self.browse.directory)
+        self.browse_button = tk.Button(self, text="Browse", command=self.browse_directory)
         self.browse_button.pack()
 
-        self.run_button = tk.Button(self, text="Find Duplicates", command=self.run_duplicates_finder)
+        self.run_button = tk.Button(self, text="Find Duplicates", command=self.pdf_duplicates.run)
         self.run_button.pack(pady=20)
+
 
     def browse_directory(self):
         folder_selected = filedialog.askdirectory()
+        if folder_selected:
+            self.pdf_duplicates.destination = folder_selected
+    def run_duplicates_finder(self):
+        if not self.pdf_duplicates.destination:
+            messagebox.showwarning("Warning", "Please select a directory first!")
         self.pdf_duplicates.run()
-        tk.messagebox.showinfo("Info", "Operation completed")
+        messagebox.showinfo("Info", "Operation completed")
 
 if __name__ == '__main__':
     app = PDFDuplicatesGUI(pdf_duplicates.pdfDuplicates())
